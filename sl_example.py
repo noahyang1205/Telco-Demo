3ac190b0fa79186e35dda5bf3152710bd95cddd7
Yang <Hao-Yu.Yang@bain.com>
2023-03-02 11:10:43 -0500

    added sidebar, code cleanup

--- sl_example.py
+++ sl_example.py
@@ -5,7 +5,7 @@ import openai
 from pathlib import Path
 from io import StringIO
 from pathlib import Path
-
+import imageio as iio
 
 st.set_page_config(
     page_title="Telco chat-bot demo",
@@ -15,7 +15,7 @@ st.set_page_config(
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-st.header("Sarah Smith's Chat Log")
+st.header("Telco Chat Log")
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
@@ -23,65 +23,63 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
     
-    
-###############################################################
-# def query(payload):
-# 	response = requests.post(API_URL, headers=headers, json=payload)
-# 	return response.json()
-###############################################################
 
-
-uploaded_file = st.file_uploader("Choose a file")
+img = iio.imread('./assets/logo.png')
+st.sidebar.image(image=img)
+st.sidebar.markdown("# Chat-bot parameters")
+st.sidebar.write("## Upload Context File")
+uploaded_file = st.sidebar.file_uploader("Choose a context file")
 
 bd_chat_lines_context = ''
 
-if uploaded_file is None:
+if uploaded_file is None: # If no context file is provided, defaults to using the context file in directory
     bd_chat_lines_context = Path('context.txt').read_text("UTF-8")
     tokens = bd_chat_lines_context.split()
-    st.write('No context file uploaded, defaulting to Sarah Smith')
-else:
-    #bd_chat_lines_context = uploaded_file.read_text("UTF-8")
+    if len(tokens) > 900:
+        st.sidebar.write('Document token exceeds limit, truncating....')
+        tokens = tokens[:900]      
+    bd_chat_lines_context = ''.join(tokens)
+    
+    st.sidebar.write('No context file uploaded, defaulting to Sarah Smith')
+else: 
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     bd_chat_lines_context = stringio.getvalue()
     tokens = bd_chat_lines_context.split()
+    
     print(len(tokens))
-if len(tokens) > 900:
-    st.write('Document token exceeds limit, truncating....')
-    tokens = tokens[:900]
-        
+    if len(tokens) > 900:
+        st.sidebar.write('Document token exceeds limit, truncating....')
+        tokens = tokens[:900]      
     bd_chat_lines_context = ''.join(tokens)
 
-    
-with st.expander("See Tranucated ouptut"):
-    st.write(bd_chat_lines_context)
+expander = st.sidebar.expander("See uploaded context")
+expander.write(bd_chat_lines_context)
 
+st.sidebar.write('## Model Fine-tuning?')
 
-option = st.selectbox('Fine-tune model?',('No fine-tune', 'Fine-tuned'))
+ft_model_name, non_ft_model_name ='Fine-tuned model: ada:ft-bain-data-science-2023-03-02-00-53-43', 'Non-fine-tuned model: text-davinci-003'
 
-print(len(tokens))
-
-
+option = st.sidebar.selectbox('',(non_ft_model_name,ft_model_name),index=0)
+print(option)
 def query(prompt):
         
-    #query = 'Why should I choose AT&T'
-    #print(prompt['inputs']['text'])
     openAI_prompt = bd_chat_lines_context + "\n" + prompt['inputs']['text'] + "\n"
-    
-    #print(prompt)
-    
-    if option == 'No fine-tune':
+        
+    if option == non_ft_model_name:
+        print('Non ft model')
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=openAI_prompt,
             temperature=0.0,
             max_tokens=511,
             top_p=1,
-            frequency_penalty=0,
+            frequency_penalty=1.33,
             presence_penalty=0,
             stop=["Q:"],
             timeout=20
         )
-    elif 'Fine-tuned':
+    elif option == ft_model_name:
+        print('FT model')
         response = openai.Completion.create(
             model="ada:ft-bain-data-science-2023-03-02-00-53-43",
             prompt=openAI_prompt,
@@ -109,19 +107,17 @@ def get_text():
 
 
 user_input = get_text()
-#print(st.session_state.generated)
+
 if user_input:
     output = query(prompt={
         "inputs": {
             "past_user_inputs": st.session_state.past,
             "generated_responses": st.session_state.generated,
             "text": user_input,
-        },"parameters": {"repetition_penalty": 1.33}, 
+        },"parameters": {"repetition_penalty": 0}, 
     })
     generated_response = output['inputs']['generated_responses']
-    #print(f'generated responses {generated_response}')
     st.session_state.past.append(user_input)
-    #print(f'prompt {output}')
 
     try:
         st.session_state.generated.append(output["generated_text"])
@@ -130,23 +126,11 @@ if user_input:
  
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        # if len(st.session_state["past"]) == 1:
-        #     st.session_state.past.append([])
+
         print(st.session_state["generated"])
         print(st.session_state["past"])
         print(i)
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
-# if st.session_state['generated']:
-#     session_state_generated = st.session_state['generated']
-#     print(f'Session state generated {session_state_generated}')
-#     for i in range(len(st.session_state['generated'])-1, -1, -1):
-#         message(st.session_state["generated"][i], key=str(i))
-#         print(f'i: {i}')
-#         print(st.session_state["generated"])
-#         print(st.session_state['past'])
-        
-#         #if len(st.session_state['past']) > i:  
-#         message(st.session_state['past'][i-1], is_user=True, key=str(i) + '_user')
         
