dd0e7d53f1229d49bbc6e3ed16a47a5c3d87a9d6
Noah Yang <hao-yu.yang@bain.com>
2023-05-01 11:35:03 -0400

    update to gcs tts

--- pages/5_voice_support.py
+++ pages/5_voice_support.py
@@ -9,15 +9,18 @@ import os
 import pandas as pd
 import json
 import pyodbc
-#from TTS.api import TTS
+from google.cloud import texttospeech
+
+# from TTS.api import TTS
 import subprocess
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 st.set_page_config(layout="wide")
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = ""
 
-if "cycle_state" not in st.session_state:   
+if "cycle_state" not in st.session_state:
     st.session_state.cycle_state = "To run"
 
 if "user_profile" not in st.session_state:
@@ -43,29 +46,38 @@ cnxn = pyodbc.connect(
 )
 cursor = cnxn.cursor()
 
-query = "SELECT TOP 20 * FROM [dbo].[dispatch]"
+# query = "SELECT TOP 20 * FROM [dbo].[dispatch]"
 
-cursor.execute(query)
+# cursor.execute(query)
 
 user_database = pd.read_csv("./assets/dtv_profiles.csv")
+channel_prices_csv = pd.read_csv("./assets/channel_price.csv")
 
 img_business = iio.imread("./assets/DirecTV-Logo.jpg")
 st.image(image=img_business, width=400)
-img_business = iio.imread("./assets/AT&T_logo_2016.svg.png")
+img_business = iio.imread("./assets/DirecTV-Logo.jpg")
 st.sidebar.image(image=img_business, width=200)
-
+with st.sidebar:
+    with st.expander(label="See customer database"):
+        st.dataframe(data=user_database)
+    with st.expander(label="See channel database"):
+        st.dataframe(data=channel_prices_csv)
+    
 col1, col2 = st.columns(2)
 
 col1.header("User Interface")
 col2.header("Backend System")
 
 # TTS model
-#model_name = TTS.list_models()[0]
-#model_name = 'tts_models/en/ljspeech/tacotron2-DDC'
-#model_name =   'coqui_studio/en/Viktor Eka/coqui_studio'
-#tts = TTS(model_name)
+# model_name = TTS.list_models()[0]
+# model_name = 'tts_models/en/ljspeech/tacotron2-DDC'
+# model_name =   'coqui_studio/en/Viktor Eka/coqui_studio'
+# tts = TTS(model_name)
 
-def text2voice(text,col):
+# Google text to speech API setup
+client = texttospeech.TextToSpeechClient()
+
+def text2voice(text, col):
     """translate text to voice with TTS and output chat history
 
     Args:
@@ -78,24 +90,42 @@ def text2voice(text,col):
     # text = text.replace("MTV", "emme tea vee")
     # text = text.replace("ESPN", "ee esse pea en")
     # text = text.replace("AMC", "'ay emme sieh")
-    
+
     os.remove("output.wav") if os.path.exists("output.wav") else None
-    #subprocess.run(['ls'], shell=True)
-    #cmd = f'tts --text "{text}" --model_name tts_models/en/ljspeech/tacotron2-DDC --emotion Neutral --out_path output.wav'
+    # subprocess.run(['ls'], shell=True)
+    # cmd = f'tts --text "{text}" --model_name tts_models/en/ljspeech/tacotron2-DDC --emotion Neutral --out_path output.wav'
     # print(cmd)
     # os.system(cmd)
-    #tts.tts_to_file(text=text, speaker=tts.speakers[0], language=tts.languages[0], file_path="output.wav",speed=1.5,  emotion='Neutral')
-    #tts.tts_to_file(text=text,file_path="output.wav",speed=0.9,  emotion='Happy')
+    # tts.tts_to_file(text=text, speaker=tts.speakers[0], language=tts.languages[0], file_path="output.wav",speed=1.5,  emotion='Neutral')
+    # tts.tts_to_file(text=text,file_path="output.wav",speed=0.9,  emotion='Happy')
     
-    output_tts = gtts.gTTS(text)
-    output_tts.save("output.wav")
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+    
+    voice = texttospeech.VoiceSelectionParams(
+    language_code="en-US", name="en-US-Neural2-D",speaking_rate=0.89)
+    
+    audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.LINEAR16,  effects_profile_id=['large-home-entertainment-class-device']
+)
+    
+    response = client.synthesize_speech(
+    input=synthesis_input, voice=voice, audio_config=audio_config
+)
 
-    #output_tts.save("output.mp3")
-    output_audio = open("./output.wav", "rb")
+    with open("output.mp3", "wb") as out:
+    # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+    # output_tts = gtts.gTTS(text)
+    # output_tts.save("output.wav")
+
+    # output_tts.save("output.mp3")
+    output_audio = open("./output.mp3", "rb")
     col.write("System Output: ")
-    col.audio(output_audio, format="audio/wav")
-    col.text_area(label="Chat History", value=st.session_state.chat_history,height=400)
-    
+    col.audio(output_audio, format="audio/mp3")
+    col.text_area(label="Chat History", value=st.session_state.chat_history, height=400)
+
+
 def input_output_cycle():
     """One cycle of user input and AI response, a cycle consists of the following steps:
         1. Record user audio input, transcribe to text
@@ -119,7 +149,7 @@ def input_output_cycle():
         col1.write("User Input: ")
         col1.audio(audio_bytes, format="audio/wav")
 
-        JSON_prompt = f'Based on the request, create a JSON file with the same structure as the example JSON file. """\n\n\n Request: {user_input}, Example JSON: {example_query}. Possible values for Input Type field: 1. User Identification, 2. Add Channel, 3. Delete Channel 4. Channel Information, 5. Greet. Details about each field: 1. User Identification: when user provide first and last name. 2. Add Channel: When user wants to add/subscribe to a new channel. 3. Delete: When user wants to remove/unsubscribe to a channel. 4. Channel Information: When user ask about a channel. 5. Greet: When user greets."""\n\n\n'
+        JSON_prompt = f'Based on the request, create a JSON file with the same structure as the example JSON file. """\n\n\n Request: {user_input}, Example JSON: {example_query}. Possible values for Input Type field: 1. User Identification, 2. Add Channel, 3. Remove Channel 4. Channel Information, 5. Greet. Details about each field: 1. User Identification: when user provide first and last name. 2. Add Channel: When user wants to add/subscribe to a new channel. 3. Remove: When user wants to remove/unsubscribe to a channel. 4. Channel Information: When user ask about a channel. 5. Greet: When user greets."""\n\n\n'
 
         response = openai.Completion.create(
             model="text-davinci-003",
@@ -153,13 +183,14 @@ def input_output_cycle():
                 presence_penalty=0,
                 timeout=20,
             )
-            output_text = response["choices"][0]["text"].strip('\n')
+            output_text = response["choices"][0]["text"].strip("\n")
 
             st.session_state.chat_history += (
                 f"AI response: {output_text} \n"  # append AI response to chat history
             )
-            
-            text2voice(text=output_text, col=col1,tts=tts)
+
+            text2voice(text=output_text, col=col1
+                       )
             # output_tts = gtts.gTTS(output_text)
             # output_tts.save("output.wav")
             # output_audio = open("output.wav", "rb")
@@ -181,13 +212,13 @@ def input_output_cycle():
             pd_query = response["choices"][0]["text"]
             col2.write("Executed Query: User Extraction (pandas dataframe): ")
             col2.code(pd_query)
-            
+
             # Make sure local namespace variable is accessible outside the exec space
             loc = {}
             exec(pd_query, globals(), loc)
             user_profile = loc["user_profile"]
             st.session_state.user_profile = user_profile
-            
+
             col2.write("User Profile: ")
             col2.write(st.session_state.user_profile)
 
@@ -230,16 +261,20 @@ def input_output_cycle():
                 presence_penalty=0,
                 timeout=20,
             )
-            output_text = response["choices"][0]["text"].strip('\n')
+            output_text = response["choices"][0]["text"].strip("\n")
 
             st.session_state.chat_history += (
                 f"AI response: {output_text} \n"  # append AI response to chat history
             )
-            
-            text2voice(text=output_text, col=col1,tts=tts)
+
+            text2voice(text=output_text, col=col1
+                       
+                       
+                       
+)
 
         if input_JSON["Input Type"] == "Greet":
-            
+
             prompt = "You are a Direct TV customer support chat bot. Greet the user and ask for his/her first and last name in a friendly way"
 
             response = openai.Completion.create(
@@ -252,18 +287,18 @@ def input_output_cycle():
                 presence_penalty=0,
                 timeout=20,
             )
-            output_text = response["choices"][0]["text"].strip('\n')
-            if output_text[0] == '.':
+            output_text = response["choices"][0]["text"].strip("\n")
+            if output_text[0] == ".":
                 output_text = output_text[1:]
             st.session_state.chat_history += (
                 f"AI response: {output_text} \n"  # append AI response to chat history
             )
 
-            text2voice(text=output_text, col=col1, tts=tts)
+            text2voice(text=output_text, col=col1)
 
         # Channel information is in SQL to showcase our abilities to talk to different system
         if input_JSON["Input Type"] == "Channel Information":
-            
+
             original_input = input_JSON["Original Input"]
             prompt = f'Based on the question, query the mySQL database named channel_price \n """\n\n\n Question: {original_input} channel_price fields: channel, price \n"""'
             # col2.write(prompt)
@@ -285,9 +320,9 @@ def input_output_cycle():
             info = []
             for row in cursor:
                 info.append(row)
-            col2.write('Query Results')
+            col2.write("Query Results")
             col2.code(info)
-                
+
             prompt = f'Based on the question and information, inform the user in a friendly manner: \n """\n\n\n Question: {original_input}, Information: {info}\n"""'
             response = openai.Completion.create(
                 model="text-davinci-003",
@@ -299,15 +334,16 @@ def input_output_cycle():
                 presence_penalty=0,
                 timeout=20,
             )
-            
+
             output_text = response["choices"][0]["text"].strip()
 
             st.session_state.chat_history += (
                 f"AI response: {output_text} \n"  # append AI response to chat history
             )
 
-            text2voice(text=output_text, col=col1, tts=tts)
-            
+            text2voice(text=output_text, col=col1)
+
+
 if st.session_state.cycle_state == "To run":
     input_output_cycle()
 
